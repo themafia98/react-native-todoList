9a34fbc93552b87e707f3f8870a2cce8a45f2f2b
Pavel Petrovich <gaara33377@gmail.com>
2020-07-11 15:43:21 +0300

    some test changes

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.title} >Todo-list</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -18,4 +18,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  title: {
+    color: 'red',
+    fontSize: 25
+  }
 });
